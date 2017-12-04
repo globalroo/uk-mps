3704bf8493e2fd18333d629e7a4b4a95aad4e61e
Andrew Davies <globalroo@hotmail.com>
2017-12-04 01:53:03 +0000

    Updated parsing, added basic layout

--- src/App.js
+++ src/App.js
@@ -1,37 +1,87 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { formattedData } from "data/processed.js";
+import "src/App.css";
 
-const GET_ALL_MEMBERS_URL = "https://api.parliament.uk/Live/fixed-query/house_current_members?house_id=1AFu55Hs&format=application/json";
-const getMembers = async () => {
-	return await fetch(GET_ALL_MEMBERS_URL);
+class MPImage extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			src: this.props.url
+		};
+	}
+	onError = (e) => {
+		this.setState({ src: "images/404.jpeg" });
+		e.stopPropagation();
+	}
+
+	render() {
+		return <img className="mp-image" onError={this.onError} src={this.state.src} alt={this.props.alt} />;
+	}
 }
+
+const MPData = ({ id, data }) => {
+	//TODO: const { additionalName, constituency, gender, homePage, party, twitter } = data;
+	//TODO: Separate into componen file
+	const { constituency, party } = data;
+	const partyText = party._value;
+	const { label } = constituency;
+	const constituencyText = label._value;
+	return [
+		<h3 key={`${id}_party`} className="member-party">{partyText}</h3>,
+		<h3 className="member-constituency" key={`${id}_constituency`}>{constituencyText}</h3>
+	];
+};
 
 class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			mps: {}
-		}
+			mps: []
+		};
 	}
-	componentDidMount() {
+
+	async componentDidMount() {
+		// TODO: Optimise parsing - make node parse for this to consume?
+		// Serving from processed data produced by flattenData.
+		// const cachedMPs = await getCachedMembers();
+		// const mps = cachedMPs ? JSON.parse(cachedMPs) : await flattenData(this.props.members);
+		const mps = formattedData;
+		//TODO: save sorted formattedData or make util to sort based on name / place
+		mps.sort((a, b)=>{
+			if(a.familyName < b.familyName) return -1;
+			if(a.familyName > b.familyName) return 1;
+			return 0;
+		});
 		this.setState({
-			mps: getMembers()
-		})
+			mps
+		});
 	}
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          { JSON.stringify(this.state.mps) }
-        </p>
-      </div>
-    );
-  }
+
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<h1 className="App-title">Members of Parliament</h1>
+					<p className="App-intro">Sunday evening shennanigans</p>
+				</header>
+				<section className="members-of-parliament" >
+					{this.state.mps.map((mp, ix) => {
+						return (
+
+								<div className="member" key={`${mp.id}_key`}>
+									<MPImage url={mp.image} alt={mp.name} key={`${mp.id}_image`}/>
+									<div className="member-info" key={`${mp.id}_info`}>
+										<h2 key={`${mp.id}_title`}>{mp.fullNameByForename}</h2>
+										<MPData key={`${mp.id}_data`} data={mp.data} id={mp.id}/>
+									</div>
+								</div>
+
+						);
+					})}
+					</section>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import 'src/index.css';
+import App from 'src/App';
+import registerServiceWorker from 'src/registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// This image data was acquired from the excellent https://beta.parliament.uk/
+// Read the story about how the images came about here: https://pds.blog.parliament.uk/2017/07/21/mp-official-portraits-open-source-images/
+// The APIs aren't public yet but will be soon. Some amazing engineering going on over there.
+// In the meantime I've used these MP images from the beta site and stitched together with
+// the live API site (also excellent!) here: http://lda.data.parliament.uk
+// As there is no commonality other than names to make the connection I've used that.
+// Thankfully it seems pretty consistent.
+
+import { members } from "data/members";
+
+ReactDOM.render(<App members={members} />, document.getElementById('root'));
 registerServiceWorker();
